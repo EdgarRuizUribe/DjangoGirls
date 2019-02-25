75726822a99d48269261e75470690b2028c998be
EdgarRuizUribe <edgarruizuri@gmail.com>
2019-02-25 16:38:07 -0600

    Agrego funcionalidad ver detalle de cada post con url dinamica y se agrega templates de visualizacion

--- proyectoBlogGirls/blog/views.py
+++ proyectoBlogGirls/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -8,3 +8,7 @@ def post_list(request):
     # post = Post.objects.filter(published_date__lte = timezone.now())
     post = Post.objects.all()
     return render(request, 'blog/post_list.html', {'post' : post})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_details.html',{'post':post})
