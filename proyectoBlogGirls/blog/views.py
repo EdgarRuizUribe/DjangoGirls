719ca3427eadcb96c1dfc9e6c6c285520d38619e
EdgarRuizUribe <edgarruizuri@gmail.com>
2019-02-27 11:57:03 -0600

    se agrega forms.oy para agregar nuevos post desde la ui

--- proyectoBlogGirls/blog/views.py
+++ proyectoBlogGirls/blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
+from django.shortcuts import redirect
+from django.http import HttpResponseRedirect
+from django.contrib import messages
+from .forms import PostForm
 from .models import Post
 
 # Create your views here.
@@ -11,4 +15,20 @@ def post_list(request):
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_details.html',{'post':post})
+    return render(request, 'blog/post_details.html',{'post': post})
+
+def post_new(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit = False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            # messages.success(request, 'realizado')
+            # con el http response se puede redireccioner de la misma forma.
+            # return HttpResponseRedirect("/")
+            return redirect('post_detail', pk = post.pk) 
+    else:
+        form = PostForm()
+        return render(request,'blog/post_new.html',{'form' : form})
