30a53bf42811d25373d2c120623ef786681bd346
EdgarRuizUribe <edgarruizuri@gmail.com>
2019-03-05 11:08:59 -0600

    funcionalidad agregar nuevo post, editar y eliminar solo si estas logueado. prueba de estilos

--- proyectoBlogGirls/proyectoBlogGirls/settings.py
+++ proyectoBlogGirls/proyectoBlogGirls/settings.py
@@ -115,10 +115,11 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'public','static')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),'blog/static/css/',)
